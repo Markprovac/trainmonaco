9dfc7ff95ff0475e39a43d6edffb168ccfe37a60
Markprovac <mark.provac@gmail.com>
2026-09-14 10:34:15 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = "train-monaco-v1";
+var APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.map(function(key) {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  var url = event.request.url;
+
+  if (url.indexOf("trainmonaco.markrusten-e08.workers.dev") !== -1) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
